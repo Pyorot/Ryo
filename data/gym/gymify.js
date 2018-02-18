2a58bc68f8630f49e04716d1a030297319a023a2
Pyorot <FMasic@hotmail.co.uk>
2018-02-18 00:55:35 +0000

    new gyms 14/15/16/17 Feb

--- data/gym/gymify.js
+++ data/gym/gymify.js
@@ -6,6 +6,7 @@ cellDict = require('./cellDict.json')
 input = JSON.parse(fs.readFileSync('rawGyms.json'))
 gyms = {}
 
+// constructs basic gyms from rawGyms.json and cellDict.json
 for (rawGym of input) {
     let gym = {}
     let loc = rawGym[0]+','+rawGym[1]
@@ -18,9 +19,21 @@ for (rawGym of input) {
 }
 console.log("Processed", input.length, "gyms;", Object.keys(gyms).length, "unique")
 
+// sets exe property using exeGyms.csv
 exgyms = csvParse(fs.readFileSync('exeGyms.csv'))
 for (row of exgyms) {gyms[row[0].replace('/',',')].exe = true}
 console.log("Ex-labelled", exgyms.length, "gyms")
 
+// writes gyms.csv
+csvOut = ""; EOL = require('os').EOL
+for (loc in gyms) {
+    let gym = gyms[loc]
+    csvOut += loc.replace(',','/') +','
+            + gym.name.replace(/,/g,'|') +','
+            + gym.c12[0]+'/'+gym.c12[1] +','
+            + gym.exe +EOL
+}
+
 fs.writeFileSync('../gyms.json', JSON.stringify(gyms, null, 4))
+fs.writeFileSync('gyms.csv', csvOut)
 console.log("Done")
